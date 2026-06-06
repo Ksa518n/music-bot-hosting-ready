0d402cec712d2ed87504c4a386a3c5be45c59b50
Ksa518n <1.59606491e+08+Ksa518n@users.noreply.github.com>
2026-06-06 13:53:38 +0000

    Initial commit: Wick Music Bot with environment variables support

---
+++ config.js
@@ -0,0 +1,34 @@
+/*
+██╗    ██╗██╗ ██████╗██╗  ██╗    ███████╗████████╗██╗   ██╗██████╗ ██╗ ██████╗ 
+██║    ██║██║██╔════╝██║ ██╔╝    ██╔════╝╚══██╔══╝██║   ██║██╔══██╗██║██╔═══██╗
+██║ █╗ ██║██║██║     █████╔╝     ███████╗   ██║   ██║   ██║██║  ██║██║██║   ██║
+██║███╗██║██║██║     ██╔═██╗     ╚════██║   ██║   ██║   ██║██║  ██║██║██║   ██║
+╚███╔███╔╝██║╚██████╗██║  ██╗    ███████║   ██║   ╚██████╔╝██████╔╝██║╚██████╔╝
+ ╚══╝╚══╝ ╚═╝ ╚═════╝╚═╝  ╚═╝    ╚══════╝   ╚═╝    ╚═════╝ ╚═════╝ ╚═╝ ╚═════╝ 
+Copyright (c) 2024 Wick Studio
+*/
+
+module.exports = {    
+    token: process.env.TOKEN || "", // token
+    clientId: process.env.CLIENT_ID || "", // bot id
+    prefix: process.env.PREFIX || "!", // prefix
+    language: process.env.LANGUAGE || "ar", // ar for arabic | en for english
+    verbose: true,
+    musicCardPath: "./musicard.png",
+    enableLogging: true,
+    djRoleName: "Wick",
+    aliases: {
+      play: ["p", "start", "playmusic"],
+      pause: ["hold", "stopmusic"],
+      resume: ["r", "continue"],
+      skip: ["s", "next", "jump"],
+      stop: ["end", "terminate"],
+      volumeUp: ["vup", "increasevolume"],
+      volumeDown: ["vdown", "decreasevolume"],
+      repeat: ["loop"],
+      queue: ["q"],
+      nowplaying: ["np"],
+      clear: ["c"],
+      remove: ["rm", "delete"]
+  }
+};
